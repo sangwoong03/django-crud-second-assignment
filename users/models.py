596d281ef22a13f78147acc3b2a5ad10e7ca474c
sangwoong03 <sangwoong03@DESKTOP-19GVLRN.localdomain>
2022-06-12 23:33:58 +0900

    ADD: completed practice for URLconf-Views-Modelling for sign-up function

--- users/models.py
+++ users/models.py
@@ -4,10 +4,11 @@ from django.db import models
 class User(models.Model) :
   last_name     = models.CharField(max_length=16)
   first_name    = models.CharField(max_length=45)
-  email         = models.EmailField(max_length=255)
+  email         = models.EmailField(max_length=255, unique=True)
+  account       = models.CharField(max_length=45, null=False)
   password      = models.CharField(max_length=128)
-  phone_number  = models.CharField(max_length=16)
-  date_of_birth = models.DateField()
+  phone_number  = models.CharField(max_length=32)
+  date_of_birth = models.DateField(null=True)
 
   class Meta :
     db_table = "users"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json, re
+
+from django.http import JsonResponse
+from django.views import View
+
+from users.models import User
+
+class SignUpView(View) :
+
+  def post(self, request) :
+     
+    try :
+      data  = json.loads(request.body)
+      email = data["email"]
+      pw    = data["password"]
+      
+      EMAIL_CHECK = "^[a-zA-Z0-9+_.]+@[a-zA-Z0-9-.]+$"
+      PW_CHECK    = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$"
+      
+      # 이메일 중복 확인
+      if User.objects.filter(email = email).exists() :
+        return JsonResponse({"Message" : "THIS_EMAIL_ALEADY_EXIST"}, status = 400)
+
+      # 이메일에 "@" "." 여부 확인
+      if not re.match(EMAIL_CHECK, email) :
+        return JsonResponse({"Message": "INVALID EMAIL"}, status=401)
+
+      # 비밀번호가 8자리 이상의 문자, 숫자, 특수문자 포함 확인
+      if not re.match(PW_CHECK, pw) :
+        return JsonResponse({"Message": "INVALID PASSWORD"}, status=401)
+
+      User.objects.create(
+        last_name     = data["last_name"],
+        first_name    = data["first_name"],
+        account       = data["account"],
+        phone_number  = data["phone_number"],
+        date_of_birth = data["date_of_birth"],
+        email         = email,
+        password      = pw
+      )
+      
+      return JsonResponse({"Message": "SUCCESS"}, status=201)
+
+    except KeyError:
+      return JsonResponse({"Message": "KEY_ERROR"}, status=400)
+      
