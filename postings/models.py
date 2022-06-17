08bffedfc28874c0f532326f02b79540276f212e
sangwoong03 <sangwoong03@gmail.com>
2022-06-17 11:51:00 +0900

    UPDATE: modified modelling of positngs

--- postings/models.py
+++ postings/models.py
@@ -3,10 +3,16 @@ from django.db import models
 from core.models import TimeStampModel
 from users.models import User
 
-class Posting(TimeStampModel) :
+class Post(TimeStampModel) :
     content   = models.TextField()
-    image_url = models.URLField(max_length=2000)
     user      = models.ForeignKey(User, on_delete=models.CASCADE)
     
     class Meta:
-        db_table = "postings"
+        db_table = "posts"
+
+class Image(TimeStampModel) :
+    image_url = models.URLField(max_length=2000)
+    post      = models.ForeignKey("POST", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "images"
