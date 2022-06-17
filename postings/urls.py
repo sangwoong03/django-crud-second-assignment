30bcf31ebbac329e59079c2a6355d5cab1055f09
sangwoong03 <sangwoong03@gmail.com>
2022-06-18 00:18:05 +0900

    ADD: comment from modelling to views

--- postings/urls.py
+++ postings/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from postings.views import PostView, PostSearchView
+from postings.views import PostView, PostSearchView, CommentView, CommentSearchView
 
 urlpatterns = [
     path("", PostView.as_view()),
-    path("/search/<int:user_id>", PostSearchView.as_view())
+    path("/search/<int:user_id>", PostSearchView.as_view()),
+    path("/comment", CommentView.as_view()),
+    path("/comment/search/<int:post_id>", CommentSearchView.as_view()),
 ]
--- postings/views.py
+++ postings/views.py
@@ -1,11 +1,11 @@
+from ast import JoinedStr
 import json
-from lib2to3.pgen2 import token
 
 from django.http import JsonResponse
 from django.views import View
 
 from users.models import User
-from .models import Post, Image
+from .models import Post, Image, Comment
 
 from users.utils import token_decorator
 
@@ -32,7 +32,7 @@ class PostView(View) :
 
             return JsonResponse({"message": "POSTED"}, status=200)
         except KeyError:
-            return JsonResponse({"message": "ERRORRRRRR"}, status=400)
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
     def get(self, request):
         post_list = [
@@ -46,10 +46,8 @@ class PostView(View) :
         return JsonResponse({"data" : post_list}, status=200)
 
 class PostSearchView(View) :
-    # @token_decorator
     def get(self, request,user_id):
         try:
-            # user = request.user
             user = User.objects.get(id = user_id)
             post_list = [
                 {
@@ -62,5 +60,59 @@ class PostSearchView(View) :
             
             return JsonResponse({"data" : post_list}, status=200)
         
-        except :
+        except User.DoesNotExist:
             return JsonResponse({"message" : "INVALID_USER"}, status=404)
+
+class CommentView(View):
+    @token_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            post = Post.objects.get(id=data["post"])
+            user = request.user
+            
+            comment_contents = data["comment_content"]
+            
+            for comment_content in comment_contents :
+                Comment.objects.create(
+                    comment_content = comment_content,
+                    post = post,
+                    user = user
+                )
+            return JsonResponse({"message": "Comment Posted"}, status=201)
+        except Post.DoesNotExist:
+            return JsonResponse({"message": "INVALID_POST"}, status=400)
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+    def get(self, request) :
+        comments = Comment.objects.all()
+
+        comment_list = [
+            {
+                "commet_content": comment.comment_content,
+                "user"          : comment.user.account,
+                "post"          : {
+                    "content": comment.post.content
+                }
+            } for comment in comments
+        ]
+        return JsonResponse({"data": comment_list}, status=200)
+
+class CommentSearchView(View): 
+    def get(self, request, post_id) :
+        try:
+            post = Post.objects.get(id = post_id)
+
+            comment_list = [
+                {
+                    "account"        : User.objects.get(id = comment.user.id).account,
+                    "commet_content" : comment.comment_content,
+                    "created_date"   : comment.created_at 
+                } for comment in Comment.objects.filter(post_id = post.id)
+            ]
+
+            return JsonResponse({"data": comment_list}, status=200)
+        
+        except Post.DoesNotExist :
+            return JsonResponse({"message": "INVALID_POST"})
