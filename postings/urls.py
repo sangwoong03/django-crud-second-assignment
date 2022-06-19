2dec8016c060191696fbf54964113ac49a5f9cf6
sangwoong03 <sangwoong03@gmail.com>
2022-06-19 17:00:27 +0900

    ADD: added LikeView URLconf

--- postings/urls.py
+++ postings/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from postings.views import PostView, PostSearchView, CommentView, CommentSearchView
+from postings.views import LikeView, PostView, PostSearchView, CommentView, CommentSearchView
 
 urlpatterns = [
     path("", PostView.as_view()),
     path("/search/<int:user_id>", PostSearchView.as_view()),
     path("/comment", CommentView.as_view()),
     path("/comment/search/<int:post_id>", CommentSearchView.as_view()),
+    path("/like", LikeView.as_view())
 ]
